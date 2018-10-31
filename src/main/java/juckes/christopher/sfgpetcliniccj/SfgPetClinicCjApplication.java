ecafa2267645ccceabed5b82e46c43811a552609
Chris <christopher.juckes@bjss.com>
2018-10-31 12:20:51 +0000

    Initial setup commit

---
+++ src/main/java/juckes/christopher/sfgpetcliniccj/SfgPetClinicCjApplication.java
@@ -0,0 +1,12 @@
+package juckes.christopher.sfgpetcliniccj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicCjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicCjApplication.class, args);
+    }
+}
